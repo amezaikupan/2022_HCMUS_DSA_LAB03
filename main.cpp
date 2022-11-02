7ee81b0abb47c5017c8305baee1eb2f37ca0a753
Wendiago <phungngoctqk@gmail.com>
2022-11-02 22:36:45 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include "Command/command.h"
-#include "3vongfor.h"
+#include <fstream>
+#include <string>
+#include "Command04.h"
+#include "Command05.h"
+#include "DataGenerator.h"
+#include "Resource.h"
+#include <chrono>
 
-int main(int argc, char** argv){
-    ChooseCommand(argc, argv);
-    //Running();
+using namespace std;
+int main(int argc, char** argv)
+{
+    // ofstream of ("data3.txt");
+
+    // int *a = new int[10000];
+    
+    // GenerateData(a, 10000, 3);
+
+    // of << 10000 << endl;
+
+    // for(int i = 0; i < 10000; i++){
+    //     of << a[i] << " ";
+    // }
+
+    // of.close();
+    //Command04(argv[2],argv[3],argv[4]);
+    Command05(argv[2], argv[3], argv[4], argv[5]);
+    return 0;
 }
