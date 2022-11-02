0d2f547a07726e26753736a0803b5a26f592911c
Wendiago <phungngoctqk@gmail.com>
2022-11-02 15:19:52 +0700

    temp

--- Command/command.cpp
+++ Command/command.cpp
@@ -13,13 +13,22 @@ void ChooseCommand(int argc, char** argv){
     if(argc == 5){
         if(strcmp(argv[1], "-a") == 0){
             if(atoi(argv[3]) == 0){
+                cout << "ALGORITHM MODE" << endl;
+                cout << "Algorithm: " << argv[2] << endl;
+                cout << "Input file: " << argv[3] << endl;
+                cout << "Input size: " << endl;
+                cout << "--------------------------------" << endl;
                 command01(argv[2], argv[3], argv[4]);
             }else{
+                cout << "ALGORITHM MODE" << endl;
+                cout << "Algorithm: " << argv[2] << endl;
+                cout << "Input file: " << endl;
+                cout << "Input size: " << argv[3] << endl;
+                cout << "--------------------------------" << endl;
                 command03(argv[2], argv[3], argv[4]);
             }
         }
         else if(strcmp(argv[1], "-c") == 0){
-            
             Command04(argv[2],argv[3],argv[4]);
         }else{
             cout << "INVALID COMMAND" << endl;
@@ -27,6 +36,11 @@ void ChooseCommand(int argc, char** argv){
     }
     else if(argc == 6){
         if(strcmp(argv[1], "-a") == 0){
+            cout << "ALGORITHM MODE" << endl;
+            cout << "Algorithm: " << argv[2] << endl;
+            cout << "Input file: " << endl;
+            cout << "Input size: " << argv[3] << endl;
+            cout << "--------------------------------" << endl;
             command02(argv[2], argv[3], argv[4], argv[5]);
         }
         else if(strcmp(argv[1], "-c") == 0){
@@ -34,7 +48,5 @@ void ChooseCommand(int argc, char** argv){
         }else{
             cout << "INVALID COMMAND" << endl;
         }
-    }else{
-            cout << "INVALID PARAMETER" << endl;
     }
 }
