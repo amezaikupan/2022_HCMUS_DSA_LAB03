8585cf114d3b1cb795e268e481106dffcd4fb4ea
Wendiago <phungngoctqk@gmail.com>
2022-11-02 22:44:01 +0700

    temp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include "Command04.h"
-#include "Command05.h"
-#include "DataGenerator.h"
-#include "Resource.h"
-#include <chrono>
+#include "Command/command.h"
+#include "3vongfor.h"
 
-using namespace std;
-int main(int argc, char** argv)
-{
-    // ofstream of ("data3.txt");
-
-    // int *a = new int[10000];
-    
-    // GenerateData(a, 10000, 3);
-
-    // of << 10000 << endl;
-
-    // for(int i = 0; i < 10000; i++){
-    //     of << a[i] << " ";
-    // }
-
-    // of.close();
-    //Command04(argv[2],argv[3],argv[4]);
-    Command05(argv[2], argv[3], argv[4], argv[5]);
-    return 0;
+int main(int argc, char** argv){
+    ChooseCommand(argc, argv);
+    //Running();
 }
