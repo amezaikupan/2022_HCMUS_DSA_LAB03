01fb66cff5bacfd8ac47987f6281ed76b2b94f04
bonkh <caokhoi20092003@gmail.com>
2022-11-08 09:19:59 +0700

    hihi

--- Command/command03.cpp
+++ Command/command03.cpp
@@ -13,7 +13,7 @@ void printCommand03(char* algorithm, int n){
     cout << "ALGORITHM MODE" << endl;
     cout << "Algorithm: " << algorithm << endl;
     cout << "Input size: " << n << endl;
-    cout << "--------------------------------" << endl;
+    cout <<endl;
 }
 
 void command03Comp(char* algorithm, int n){
@@ -27,131 +27,99 @@ void command03Comp(char* algorithm, int n){
     int al = getAlgorithm(algorithm);
 
     //Process Command
-    switch (al)
-    {
-    case 1:
-        //Selection Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            SelectionSortComp(a, n, comp[i]);          
-        }
-
-        break;
-    case 2:
-        //Insertion Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            InsertionSortComp(a, n, comp[i]);
-        }
-        break;
-    case 3:
-        //Bubble Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            BubbleSortComp(a, n, comp[i]);            
-        }
-        break;
-    case 4:
-        //Shaker Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            ShakerSortComp(a, n, comp[i]);            
-        }
-        
-        break;
-    case 5:
-        //Shell Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            ShellSortComp(a, n, comp[i]);            
-        }
-
-        break;
-    case 6:
-        //Heap Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            HeapSortComp(a, n, comp[i]);
-        }
-
-        break;
-    case 7:
-        //Merge Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            MergeSortComp(a, n, comp[i]);
-
-            
-        }
-
-        break;
-    case 8:
-        //Quick Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            QuickSortComp(a, n, comp[i]);
-
-        }
-
-        break;
-    case 9:
-        //Counting Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            CountingSortCom(a, n, comp[i]);
-        }
-        break;
-    case 10:
-        //Radix Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            RadixSortComp(a, n, comp[i]);
-        }
-        
-        break;
-    case 11:
-        //Flash Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            FlashSortComp(a, n, comp[i]);
-        }
-        break;
-    default:
-        //Error
-        cout << "INVALID ALGORITHM NAME" << endl;
-        return;
-        
-    }
     
     printCommand03(algorithm, n);
-    for(int i = 0; i < 4; i++){        
-        cout << endl;
-        cout << "Input Order: " << getOrderName(i) << endl;
+    cout << endl;
+    for(int i = 0; i < 4; i++)
+    {
+        GenerateData(a,n,i);
+        
+        switch (i)
+        {
+        case 0:
+            writeData("input_1.txt", a, n);
+            
+            break;
+        case 1:
+            writeData("input_3.txt", a, n);
+            
+            break;
+        case 2:
+            writeData("input_4.txt", a, n);
+            
+        case 3:
+            writeData("input_2.txt", a, n);
+            
+            break;
+        }
+        int al = getAlgorithm(algorithm);
+        switch (al)
+        {
+        case 1:
+            //Selection Sort
+            SelectionSortComp(a, n, comp[i]);
+            break;
+        case 2:
+            //Insertion Sort
+            InsertionSortComp(a, n, comp[i]);
+            break;
+        case 3:
+            //Bubble Sort
+            
+            BubbleSortComp(a, n, comp[i]);
+
+            break;
+        case 4:
+            //Shaker Sort
+            ShakerSortComp(a, n, comp[i]);
+            
+            
+            break;
+        case 5:
+            //Shell Sort
+           ShellSortComp(a, n, comp[i]);
+
+            break;
+        case 6:
+            //Heap Sort
+            HeapSortComp(a, n, comp[i]);
+            
+            break;
+        case 7:
+            //Merge Sort
+            MergeSortComp(a, n, comp[i]);
+
+            break;
+        case 8:
+            //Quick Sort
+            QuickSortComp(a, n, comp[i]);
+
+            break;
+        case 9:
+            //Counting Sort
+            CountingSortCom(a, n, comp[i]);
+            break;
+        case 10:
+            //Radix Sort
+            RadixSortComp(a, n, comp[i]);
+            
+            break;
+        case 11:
+            //Flash Sort
+            FlashSortComp(a, n, comp[i]);
+            break;
+        default:
+            //Error
+            cout << "INVALID ALGORITHM NAME" << endl;
+            return;
+            
+        }
+        cout << "Input order: " << getOrderName(i) << endl;
         cout << "-------------------------------------" << endl;
         cout << "Comparisons: "  << comp[i] << endl;
+        cout << endl;
     }
+   
 
     delete[] a;
 
@@ -163,134 +131,101 @@ void command03Time(char* algorithm, int n){
     double time[4] = {0, 0, 0, 0};
     string file;
 
-    //Get algorithm 
-    int al = getAlgorithm(algorithm);
+    printCommand03(algorithm, n);  
+    cout << endl;
+    for(int i = 0; i < 4; i++)
+    {
+        GenerateData(a,n,i);
+        switch (i)
+        {
+        case 0:
+            writeData("input_1.txt", a, n);
+            
+            break;
+        case 1:
+            writeData("input_3.txt", a, n);
+            
+            break;
+        case 2:
+            writeData("input_4.txt", a, n);
+            
+        case 3:
+            writeData("input_2.txt", a, n);
+            
+            break;
+        }
+         int al = getAlgorithm(algorithm);
 
     //Process Command
-    switch (al)
-    {
-    case 1:
-        //Selection Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+        switch (al)
+        {
+        case 1:
+            //Selection Sort
             SelectionSortTime(a, n, time[i]);
-        }
-
-        break;
-    case 2:
-        //Insertion Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            break;
+        case 2:
+            //Insertion Sort
             InsertionSortTime(a, n, time[i]);
-        }
-        break;
-    case 3:
-        //Bubble Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            break;
+        case 3:
+            //Bubble Sort
+            
             BubbleSortTime(a, n, time[i]);
 
-        }
-        break;
-    case 4:
-        //Shaker Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+
+            break;
+        case 4:
+            //Shaker Sort
             ShakerSortTime(a, n, time[i]);
-        }
-        
-        break;
-    case 5:
-        //Shell Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-            ShellSortTime(a, n, time[i]);
+            
+            
+            break;
+        case 5:
+            //Shell Sort
+           ShellSortTime(a, n, time[i]);
 
-        }
-
-        break;
-    case 6:
-        //Heap Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);  
+            break;
+        case 6:
+            //Heap Sort
             HeapSortTime(a, n, time[i]);
-        }
-
-        break;
-    case 7:
-        //Merge Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            
+            break;
+        case 7:
+            //Merge Sort
             MergeSortTime(a, n, time[i]);
-        }
 
-        break;
-    case 8:
-        //Quick Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            break;
+        case 8:
+            //Quick Sort
             QuickSortTime(a, n, time[i]);
-        }
 
-        break;
-    case 9:
-        //Counting Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            break;
+        case 9:
+            //Counting Sort
             CountingSortTime(a, n, time[i]);
-        }
-        break;
-    case 10:
-        //Radix Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            break;
+        case 10:
+            //Radix Sort
             RadixSortTime(a, n, time[i]);
-        }
-        
-        break;
-    case 11:
-        //Flash Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
+            
+            break;
+        case 11:
+            //Flash Sort
             FlashSortTime(a, n, time[i]);
+            break;
+        default:
+            //Error
+            cout << "INVALID ALGORITHM NAME" << endl;
+            return;
+            
         }
-        break;
-    default:
-        //Error
-        cout << "INVALID ALGORITHM NAME" << endl;
-        return;
-        
-    }
-
-    printCommand03(algorithm, n);
-    for(int i = 0; i < 4; i++){        
-        cout << endl;
-        cout << "Input Order: " << getOrderName(i) << endl;
+        cout << "Input order: " << getOrderName(i) << endl;
         cout << "-------------------------------------" << endl;
         cout << "Running time: " << time[i] << endl;
+        cout << endl;
     }
+    
+
 
     delete[] a;
 
@@ -300,181 +235,109 @@ void command03Both(char* algorithm, int n){
     int *a = new int[n];
     int *b = new int[n];
     long long comp[4] = {0, 0, 0, 0};
-    double time[4];
+    double time[4] = {0, 0, 0, 0};
     string file;
 
-
-    //Get algorithm 
-    int al = getAlgorithm(algorithm);
-
-    //Process Command
-    switch (al)
-    {
-    case 1:
-        //Selection Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);\
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            SelectionSortComp(a, n, comp[i]);
-            SelectionSortTime(b, n, time[i]);
-        }
-
-        break;
-    case 2:
-        //Insertion Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            InsertionSortComp(a, n, comp[i]);
-            InsertionSortTime(b, n, time[i]);
-        }
-        break;
-    case 3:
-        //Bubble Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            BubbleSortComp(a, n, comp[i]);
-            BubbleSortTime(b, n, time[i]);
-        }
-        break;
-    case 4:
-        //Shaker Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            ShakerSortComp(a, n, comp[i]);
-            ShakerSortTime(b, n, time[i]);
-        }
-        break;
-    case 5:
-        //Shell Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            ShellSortComp(a, n, comp[i]);
-            ShellSortTime(b, n, time[i]);
-        }
-
-        break;
-    case 6:
-        //Heap Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            HeapSortComp(a, n, comp[i]);
-            HeapSortTime(b, n, time[i]);
-        }
-
-        break;
-    case 7:
-        //Merge Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            MergeSortComp(a, n, comp[i]);
-            MergeSortTime(b, n, time[i]);
-        }
-
-        break;
-    case 8:
-        //Quick Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            QuickSortComp(a, n, comp[i]);
-            QuickSortTime(b, n, time[i]);
-        }
-
-        break;
-    case 9:
-        //Counting Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            CountingSortCom(a, n, comp[i]);
-            CountingSortTime(b, n, time[i]);
-        }
-        break;
-        break;
-    case 10:
-        //Radix Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            RadixSortComp(a, n, comp[i]);
-            RadixSortTime(b, n, time[i]);
-        }
-        
-        break;
-    case 11:
-        //Flash Sort
-        for(int i = 0; i < 4; i++){
-            GenerateData(a, n, i);
-            b = copyData(a, b, n);
-            
-            file = "input_" + to_string(i) + ".txt";
-            writeData(file, a, n);
-
-            FlashSortComp(a, n, comp[i]);
-            FlashSortTime(b, n, time[i]);
-        }
-        break;
-    default:
-        //Error
-        cout << "INVALID ALGORITHM NAME" << endl;
-        return;
-    }
-    
     printCommand03(algorithm, n);
+    cout << endl;
+
+    for(int i = 0; i < 4; i++)
+    {
+        GenerateData(a,n,i);
+        b = copyData(a, b, n);
+        switch (i)
+        {
+        case 0:
+            writeData("input_1.txt", a, n);
     
-    for(int i = 0; i < 4; i++){     
-        cout << endl;
-        cout << "Input Order: " << getOrderName(i) << endl;
+            break;
+        case 1:
+            writeData("input_3.txt", a, n);
+            
+            break;
+        case 2:
+            writeData("input_4.txt", a, n);
+           
+        case 3:
+            writeData("input_2.txt", a, n);
+            
+            break;
+        }
+        int al = getAlgorithm(algorithm);
+
+        switch (al)
+        {
+        case 1:
+            //Selection Sort
+            SelectionSortTime(a, n, time[i]);
+            SelectionSortComp(b, n, comp[i]);
+            break;
+        case 2:
+            //Insertion Sort
+            InsertionSortTime(a, n, time[i]);
+            InsertionSortComp(b, n, comp[i]);
+            break;
+        case 3:
+            //Bubble Sort
+            
+            BubbleSortTime(a, n, time[i]);
+            BubbleSortComp(b, n, comp[i]);
+
+            break;
+        case 4:
+            //Shaker Sort
+            ShakerSortTime(a, n, time[i]);
+            ShakerSortComp(b, n, comp[i]);
+            
+            break;
+        case 5:
+            //Shell Sort
+            ShellSortTime(a, n, time[i]);
+            ShellSortComp(b, n, comp[i]);
+            break;
+        case 6:
+            //Heap Sort
+            HeapSortTime(a, n, time[i]);
+            HeapSortComp(b, n, comp[i]);
+            break;
+        case 7:
+            //Merge Sort
+            MergeSortTime(a, n, time[i]);
+            MergeSortComp(b, n, comp[i]);
+            break;
+        case 8:
+            //Quick Sort
+            QuickSortTime(a, n, time[i]);
+            QuickSortComp(b, n, comp[i]);
+
+            break;
+        case 9:
+            //Counting Sort
+            CountingSortTime(a, n, time[i]);
+            CountingSortCom(b, n, comp[i]);
+            break;
+        case 10:
+            //Radix Sort
+            RadixSortTime(a, n, time[i]);
+            RadixSortComp(b, n, comp[i]);
+            break;
+        case 11:
+            //Flash Sort
+            FlashSortTime(a, n, time[i]);
+            FlashSortComp(b, n, comp[i]);
+            break;
+        default:
+            //Error
+            cout << "INVALID ALGORITHM NAME" << endl;
+            return;
+        }
+        cout << "Input order: " << getOrderName(i) << endl;
         cout << "-------------------------------------" << endl;
         cout << "Running time: " << time[i] << endl;
-        cout << "Comparisons: "  << comp[i] << endl;
+        cout << "Comparisons: " << comp[i] << endl;
+        cout << endl;
     }
+    
     
     delete[] a;
     delete[] b;
