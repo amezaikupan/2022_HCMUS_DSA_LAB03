cf2ba3c1cd2cc366f90a0670e7a86f830721e6e7
amezaiku <amezaikupan@gmail.com>
2022-11-08 09:59:23 +0700

    Ver final.2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "Command/command.h"
-#include "3ForLoop/3ForLoop.h"
+// #include "3ForLoop/3ForLoop.h"
 
 int main(int argc, char** argv){
+
+    //1. Run file main.exe to run command 1 -> 5
     ChooseCommand(argc, argv);
+    //2. Run file 3_for_loop.exe to run 3 for loop on all data order and algorithms
     // Running();
+    
 }
